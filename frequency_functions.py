4b10fda1d7924ba311d5375a6e595225f69c454c
physerikc <physerikc@gmail.com>
2025-06-04 18:42:17 -0300

    update

--- frequency_functions.py
+++ frequency_functions.py
@@ -7,21 +7,22 @@ import numpy as np
 import datetime
 import matplotlib.dates as mdates
 
-def call_function():
-    list_glitches = []
-    n = int(input("glitches_number = Quantos glithes você quer escolher? "))
+# def call_function():
+#     list_glitches = []
+#     n = int(input("glitches_number = Quantos glithes você quer escolher? "))
 
-    for i in range(n):
-        glitch = input(f"\nDigite o glitch {i+1}: ")
-        list_glitches.append(glitch)
+#     for i in range(n):
+#         glitch = input(f"\nDigite o glitch {i+1}: ")
+#         list_glitches.append(glitch)
 
-    print(f'\n{list_glitches}')
+#     print(f'\n{list_glitches}')
     
-    x_letter = input(f"\nDigite a letra correspondente do eixo x: \n 'd' for 'daily',\n 'w' for 'weekly',\n 'm' for 'monthly',\n 'q' for 'quarterly',\n '2q' for 'half-yearly',\n 'y' for 'annual'\n")
+#     x_letter = input(f"\nDigite a letra correspondente do eixo x: \n 'd' for 'daily',\n 'w' for 'weekly',\n 'm' for 'monthly',\n 'q' for 'quarterly',\n '2q' for 'half-yearly',\n 'y' for 'annual'\n")
 
-    return n, list_glitches, x_letter
+#     return n, list_glitches, x_letter
 
 def inputs_dependences(data, n, glitches_chosen, chave):
+    
     freq_dict = dict([
         ('d', 'daily'),
         ('w', 'weekly'),
@@ -31,36 +32,21 @@ def inputs_dependences(data, n, glitches_chosen, chave):
         ('y', 'annual')
     ])
     if chave not in freq_dict:
-        print("Letra inválida.")
+        print("Invalid key.")
         return
-        
-    # for i in range(n):
-    #     if glitches_chosen[i] not in data['label']:
-    #         print("Glitch inválido.")
-    
-    # Verification
     
     valid_labels = set(data['label'].unique())
     invalid_labels = [nome for nome in glitches_chosen if nome not in valid_labels]
     
     if invalid_labels:
-        print("Os seguintes nomes são inválidos ou foram digitados incorretamente:")
+        print("The following names are invalid or misspelled: \n")
         for nome in invalid_labels:
             print(f"  - {nome}")
     else:
-        print("Todos os nomes escolhidos são válidos!")
-
-    # valid_labels = data['label'].unique()
-
-    # invalidos = []
-    # for nome in glitches_chosen:
-    #     if nome not in valid_labels:
-    #         print(f"Atenção: {nome} não está no dataframe atual, mas será aceito.")
-            # Você pode ainda decidir: invalidos.append(nome) ou ignorar
-
-    # Continue com a lógica desejada
+        print("All chosen names are valid.")
 
 def plot_function(data, glitches_number, glitches_chosen, time_key):
+    
     freq_dict = dict([
         ('d', 'daily'),
         ('w', 'weekly'),
@@ -84,10 +70,35 @@ def plot_function(data, glitches_number, glitches_chosen, time_key):
 
     plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%Y-%m'))
     plt.gcf().autofmt_xdate()
-        
+
     plt.ylabel('Numbers of Glitches')
     plt.title(f'O3a run events {freq_dict[time_key]}')
     plt.legend()
     plt.grid()
     plt.tight_layout()
+
+    plt.savefig('images/time_tracking.png')
     plt.show()
+
+def count_glitch(data, time, glitch):
+
+    # tam = len(data)
+    # time_begin = data['GPStime'][0]
+    # time_end = data['GPStime'][tam-1]
+    # time_range = time_end - time_begin
+    
+    # hours_number = int(time_range/3600)
+    
+    print(time)
+    bins = time
+    
+    # Filtra apenas os eventos do tipo de glitch desejado
+    data_filtered = data[data['label'] == glitch]
+    
+    # Divide os tempos filtrados em faixas
+    data_binned = pd.cut(data_filtered['GPStime'], bins=bins)
+    
+    # Conta quantos eventos por faixa
+    counts = data_binned.value_counts().sort_index()
+    
+    return counts
